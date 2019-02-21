21740e0181c617d3d14da9c20efcec9f1e35b606
Patrik Mada <patrik.mada@firma.seznam.cz>
2019-02-21 19:30:57 +0100

    feat: Adds adjecent cells recognition

--- src/main/scala/Cell.scala
+++ src/main/scala/Cell.scala
@@ -1,16 +1,30 @@
-case class Cell(row: Int, column: Int, links: List[Cell]) {
+case class Cell(row: Int, column: Int, adjacentCells:List[Option[Cell]], links: List[Cell]) {
 
   def link(that: Cell, bidirectional: Boolean = true): Cell = {
     if (bidirectional) that.link(this, bidirectional = false)
-    Cell(this.row, this.column, that :: links)
+    this.copy(links = that :: links)
   }
 
   def unlink(that: Cell, bidirectional: Boolean = true): Cell = {
     if (bidirectional) that.unlink(this, bidirectional = false)
-    Cell(this.row, this.column, links.filter(_ != that))
+    this.copy(links = links.filter(_ != that))
   }
 
   def linked(that: Cell): Boolean =
     this.links.contains(that)
+
+  def getAdjacentCells(grid: Grid): Cell = {
+
+    def getNeighbour(i: Int, j: Int): Option[Cell] =
+      if ((0 to grid.rows contains i) && (0 to grid.columns contains j)) Some(grid(i)(j))
+      else None
+
+    val seqOfAdjacent = for {
+      i <- this.row -1 to this.row + 1 by 2
+      j <- this.row -1 to this.row + 1 by 2
+    } yield getNeighbour(i, j)
+
+    this.copy(adjacentCells = seqOfAdjacent.toList)
+  }
 
 }
