60647e28773d0d0285a606ca54fbb9822e68ef85
Patrik Mada <patrik.mada@firma.seznam.cz>
2019-02-22 19:06:17 +0100

    feat: Adds functionality to Grid class
    - Implements size field
    - Implements .getRandomCell method

--- src/main/scala/Grid.scala
+++ src/main/scala/Grid.scala
@@ -1,11 +1,20 @@
+import scala.util.Random
+
 case class Grid(rows: Int, columns: Int) {
 
   type Grid = Vector[Vector[Cell]]
+
+  val size: Int = rows * columns
   val grid: Grid = prepareGrid
 
   private def prepareGrid: Grid =
     Vector.tabulate(this.rows, this.columns){
       (i, j) => Cell.placeCell(i, j, this)
     }
+
+  def getRandomCell: Cell = {
+    val random = new Random()
+    this.grid(random.nextInt(rows + 1))(random.nextInt(columns + 1))
+  }
 
 }
